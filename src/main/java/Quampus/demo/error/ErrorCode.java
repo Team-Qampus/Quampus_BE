4e21c0a2465fd73be24696338f17bb0c74645568
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-02-17 08:09:06 +0900

    Delete src/main/java/Quampus/demo directory

--- src/main/java/Quampus/demo/error/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package Quampus.demo.error;
-
-import org.springframework.http.HttpStatus;
-
-public interface ErrorCode {
-    String name();
-    HttpStatus getHttpStatus();
-    String getMessage();
-}
