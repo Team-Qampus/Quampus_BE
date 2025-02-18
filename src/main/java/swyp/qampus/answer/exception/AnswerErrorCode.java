ebac3a6622673b852c4b6ac7a8f92e073d4290fd
tkv00 <tkv0098@gmail.com>
2025-02-18 13:51:07 +0900

    feat:답변 채택 서비스 구현

---
+++ src/main/java/swyp/qampus/answer/exception/AnswerErrorCode.java
@@ -0,0 +1,15 @@
+package swyp.qampus.answer.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import swyp.qampus.exception.ErrorCode;
+@RequiredArgsConstructor
+@Getter
+public enum AnswerErrorCode implements ErrorCode {
+    DUPLICATED_CHOSEN(HttpStatus.CONFLICT,"이미 채택된 답변입니다."),
+    DUPLICATED_CHOSEN_OF_QUESTION(HttpStatus.BAD_REQUEST,"이 질문에는 이미 채택된 답변이 있습니다."),
+    NOT_EXIST_ANSWER(HttpStatus.NOT_FOUND,"답변이 존재하지 않습니다.");
+    private final HttpStatus httpStatus;
+    private final String message;
+}
--- src/main/java/swyp/qampus/answer/service/AnswerServiceImpl.java
+++ src/main/java/swyp/qampus/answer/service/AnswerServiceImpl.java
@@ -7,10 +7,14 @@ import swyp.qampus.answer.domain.Answer;
 import swyp.qampus.answer.domain.AnswerRequestDto;
 import swyp.qampus.answer.domain.AnswerResponseDto;
 import swyp.qampus.answer.domain.AnswerUpdateRequestDto;
+import swyp.qampus.answer.exception.AnswerErrorCode;
 import swyp.qampus.answer.repository.AnswerRepository;
+import swyp.qampus.common.ResponseDto;
 import swyp.qampus.exception.CommonErrorCode;
+import swyp.qampus.exception.RestApiException;
 import swyp.qampus.question.domain.MessageResponseDto;
 import swyp.qampus.question.domain.Question;
+import swyp.qampus.question.exception.QuestionErrorCode;
 import swyp.qampus.user.domain.User;
 import swyp.qampus.exception.CustomException;
 import swyp.qampus.exception.ErrorCode;
@@ -60,6 +64,37 @@ public class AnswerServiceImpl implements AnswerService {
 
         answerRepository.delete(answer);
         return new MessageResponseDto("답변 삭제 성공");
+    }
+
+    @Override
+    public ResponseDto choice(Long answerId, Long questId, String token) {
+        //TODO:JWT로 교체
+        String userId=token;
+        Answer answer=answerRepository.findById(answerId).orElseThrow(
+                ()-> new RestApiException(AnswerErrorCode.NOT_EXIST_ANSWER));
+        Question question=questionRepository.findById(questId).orElseThrow(
+                ()->new RestApiException(QuestionErrorCode.NOT_EXIST_QUESTION)
+        );
+
+        //사용자 권한 검사 -> 해당 질문을 올린 유저와 일치하는가?
+        if(!userId.equals(question.getUser().getUserId())){
+            throw new RestApiException(CommonErrorCode.UNAUTHORIZED);
+        }
+
+        //해당 질문에서 이미 채택한 답변이 존재하는 경우
+        Integer exitedChosen=answerRepository.countChoiceOfAnswer(questId);
+        if(exitedChosen>=1){
+            throw new RestApiException(AnswerErrorCode.DUPLICATED_CHOSEN_OF_QUESTION);
+        }
+
+        //이미 채택된 답변에 대한 요청 시
+        if(answer.getIsChosen()){
+            throw new RestApiException(AnswerErrorCode.DUPLICATED_CHOSEN);
+        }
+
+        answer.setIsChosen(true);
+        answerRepository.save(answer);
+        return ResponseDto.of(true,200,"채택 성공");
     }
 
 }
