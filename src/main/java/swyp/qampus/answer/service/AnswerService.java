8c8fa257b141cca38803b2ddba10ee1d38557370
tkv00 <tkv0098@gmail.com>
2025-02-18 13:51:07 +0900

    feat:답변 채택 서비스 구현

--- src/main/java/swyp/qampus/answer/service/AnswerService.java
+++ src/main/java/swyp/qampus/answer/service/AnswerService.java
@@ -3,10 +3,12 @@ package swyp.qampus.answer.service;
 import swyp.qampus.answer.domain.AnswerRequestDto;
 import swyp.qampus.answer.domain.AnswerResponseDto;
 import swyp.qampus.answer.domain.AnswerUpdateRequestDto;
+import swyp.qampus.common.ResponseDto;
 import swyp.qampus.question.domain.MessageResponseDto;
 
 public interface AnswerService {
     AnswerResponseDto createAnswer(AnswerRequestDto requestDto);
     MessageResponseDto updateAnswer(Long answer_id, AnswerUpdateRequestDto requestDto);
     MessageResponseDto deleteAnswer(Long answer_id);
+    ResponseDto choice(Long answerId,Long questId,String token);
 }
