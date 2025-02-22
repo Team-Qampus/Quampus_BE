c760ff2dc044fd7ead1015b423cc087181686480
tkv00 <tkv0098@gmail.com>
2025-02-22 22:01:44 +0900

    feat:University 도메인 추가

---
+++ src/main/java/swyp/qampus/university/domain/University.java
@@ -0,0 +1,55 @@
+package swyp.qampus.university.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import swyp.qampus.user.domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class University {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "university_id",nullable = false)
+    private Long universityId;
+
+    @Column(name = "university_name",nullable = false)
+    private String universityName;
+
+    @Column(name = "weekly_choice_cnt")
+    private Integer weeklyChoiceCnt;
+
+    @Column(name = "monthly_choice_cnt")
+    private Integer monthlyChoiceCnt;
+
+    //유저랑 양방향관계
+    @OneToMany(mappedBy = "university")
+    private List<User> users=new ArrayList<>();
+
+    @Builder
+    public University(String universityName){
+        this.monthlyChoiceCnt=0;
+        this.weeklyChoiceCnt=0;
+        this.universityName=universityName;
+    }
+
+    //채택 수 증가
+    void addChoiceCnt(){
+        this.monthlyChoiceCnt++;
+        this.weeklyChoiceCnt++;
+    }
+
+    //채택 수 감소
+    void decreaseChoiceCnt(){
+        if(weeklyChoiceCnt>0){
+            weeklyChoiceCnt--;
+        }
+        if(monthlyChoiceCnt>0){
+            monthlyChoiceCnt--;
+        }
+    }
+}
