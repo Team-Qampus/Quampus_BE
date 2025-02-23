d56a60653f6308a1febadaa3d21724bb5a25e00f
tkv00 <tkv0098@gmail.com>
2025-02-24 00:47:07 +0900

    feat:대학교 상세정보 조회 쿼리 구현

--- src/main/java/swyp/qampus/university/repository/UniversityRepositoryCustom.java
+++ src/main/java/swyp/qampus/university/repository/UniversityRepositoryCustom.java
@@ -1,10 +1,15 @@
 package swyp.qampus.university.repository;
 
+import swyp.qampus.university.domain.response.UniversityDetailResponseDto;
 import swyp.qampus.university.domain.response.UniversityRankResponseDto;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UniversityRepositoryCustom {
+    //학교랭킹 조회
     Optional<List<UniversityRankResponseDto>> getUniversityRanking(Integer limit,String period);
+
+    //학교 이름으로 학교 조회 및 상세보기
+    Optional<UniversityDetailResponseDto> getUniversityDetail(String universityName);
 }
--- src/main/java/swyp/qampus/university/service/UniversityServiceImpl.java
+++ src/main/java/swyp/qampus/university/service/UniversityServiceImpl.java
@@ -1,4 +1,22 @@
 package swyp.qampus.university.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import swyp.qampus.login.util.JWTUtil;
+import swyp.qampus.university.domain.response.UniversityRankResponseDto;
+import swyp.qampus.university.repository.UniversityRepository;
+
+import java.util.List;
+import java.util.Optional;
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class UniversityServiceImpl implements UniversityService{
+    private final UniversityRepository universityRepository;
+    private final JWTUtil jwtUtil;
+    @Override
+    public Optional<List<UniversityRankResponseDto>> getUniversityRanking(String token, Integer limit, String period) {
+        return universityRepository.getUniversityRanking(limit,period);
+    }
 }
