0202f7947e898a646e42718c27e408dccc99c688
tkv00 <tkv0098@gmail.com>
2025-02-24 13:12:06 +0900

    merge:디렉토리 충돌 해결

---
+++ src/main/java/swyp/qampus/login/config/data/RedisCustom.java
@@ -0,0 +1,34 @@
+package swyp.qampus.login.config.data;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+import org.springframework.stereotype.Component;
+
+@RedisHash("redis_custom")
+@Getter
+@Component
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RedisCustom {
+
+    @Id
+    private String keyName;
+
+    @Indexed
+    private String value;
+
+    @TimeToLive
+    private int TTL;
+
+    @Builder
+    public RedisCustom( String keyName, String value,int TTL) {
+        this.keyName = keyName;
+        this.value = value;
+        this.TTL=TTL;
+    }
+}
