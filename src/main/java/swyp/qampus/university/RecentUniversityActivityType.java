57f4b384d1308064c008d8d9983e1085c1779f50
tkv00 <tkv0098@gmail.com>
2025-02-27 17:50:03 +0900

    docs:Kafka 의존성 설치

---
+++ src/main/java/swyp/qampus/university/RecentUniversityActivityType.java
@@ -0,0 +1,14 @@
+package swyp.qampus.university;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum RecentUniversityActivityType {
+    ANSWER("답변 작성"),
+    QUESTION("질문 작성"),
+    CHOICE("채택"),
+    LIKE("좋아요"),
+    CURIOUS("나도 궁금해요");
+
+    private final String message;
+}
