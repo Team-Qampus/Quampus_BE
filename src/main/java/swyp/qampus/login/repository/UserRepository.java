0f955a9fac12571575eeb955ecfc5c5af8c9c3fb
홍기문(Kimoon Hong) <153352715+KimoonH@users.noreply.github.com>
2025-02-28 18:08:13 +0900

    카카오 소셜 로그인 수정
    - existsToekn추가
    - OAuthConverter 빌더 양식 추가
    - UserConverter 가입 후 데이터 조회 추가
    - UserRepository에 EntityGraph 어노테이션 추가(대학교 이름 조회하기 위해서)
    - Complete 회원가입 로직에 JWT 토큰 유무에 따른 발급 및 유지
    - OAuthService에도 동일하게 로직 추가 및 로그 추가
    -

--- src/main/java/swyp/qampus/login/repository/UserRepository.java
+++ src/main/java/swyp/qampus/login/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package swyp.qampus.login.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import swyp.qampus.login.entity.User;
@@ -7,6 +8,6 @@ import swyp.qampus.login.entity.User;
 import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    @EntityGraph(attributePaths = {"university"})
     Optional<User> findByEmail(String email);
 }
