f40d00fcc112b7818240bc7ce7cf7f7e8e5bcfac
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-03-02 21:09:34 +0900

    Update WebOAuthSecurityConfig.java

--- src/main/java/swyp/qampus/login/config/WebOAuthSecurityConfig.java
+++ src/main/java/swyp/qampus/login/config/WebOAuthSecurityConfig.java
@@ -66,7 +66,7 @@ public class WebOAuthSecurityConfig implements WebMvcConfigurer {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // 인증 정보 포함 허용
-        config.setAllowedOrigins(List.of("http://localhost:3000","http://127.0.0.1:3000","https://qampus-fe-deploy.vercel.app")); // 허용할 프론트엔드 도메인
+        config.setAllowedOrigins(List.of("http://localhost:3000","http://127.0.0.1:3000","https://qampus-fe-deploy.vercel.app","https://kapi.kakao.com")); // 허용할 프론트엔드 도메인
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드
         config.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
         config.setMaxAge(3600L); // 모든 Origin 허용
