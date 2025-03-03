45bad3d98732dc95ad5dd4d318d09519624b4a20
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-03-03 13:24:01 +0900

    Update OpenAiConfig.java

--- src/main/java/swyp/qampus/ai/config/OpenAiConfig.java
+++ src/main/java/swyp/qampus/ai/config/OpenAiConfig.java
@@ -16,7 +16,7 @@ public class OpenAiConfig {
     @Bean(name = "openAiRestTemplate")
     public RestTemplate template(){
         SimpleClientHttpRequestFactory factory=new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(5000);
+        factory.setConnectTimeout(10000);
         factory.setReadTimeout(5000);
         RestTemplate restTemplate=new RestTemplate(factory);
         restTemplate.getInterceptors().add(((request, body, execution) -> {
