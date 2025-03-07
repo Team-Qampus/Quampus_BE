3844197829198f4942ab8c353f5a1e42f139d2af
tkv00 <tkv0098@gmail.com>
2025-03-08 06:22:51 +0900

    feat:Acivity 엔티티 생성 및 매핑

---
+++ src/main/java/swyp/qampus/activity/Activity.java
@@ -0,0 +1,28 @@
+package swyp.qampus.activity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import swyp.qampus.common.BaseEntity;
+import swyp.qampus.university.domain.University;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Activity extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "activity_id",nullable = false)
+    private Long activityId;
+
+    @Column(name = "activity_type",nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private ActivityType activityType;
+
+    @Column(name = "activity_major",nullable = false)
+    private String activityMajor;
+
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "university_id")
+    private University university;
+
+}
