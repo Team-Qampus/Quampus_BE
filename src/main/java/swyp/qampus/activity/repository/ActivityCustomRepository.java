9d473f929a4e5ed62ebfdf28afaff16ffe2835da
tkv00 <tkv0098@gmail.com>
2025-03-08 07:16:12 +0900

    feat:Activity Repositort 구현

--- src/main/java/swyp/qampus/activity/repository/ActivityCustomRepository.java
+++ src/main/java/swyp/qampus/activity/repository/ActivityCustomRepository.java
@@ -1,4 +1,9 @@
 package swyp.qampus.activity.repository;
 
+import swyp.qampus.activity.dto.RecentActivityResponseDto;
+
+import java.util.List;
+
 public interface ActivityCustomRepository {
+    List<RecentActivityResponseDto> getRecentActivityOrderByRecent(Long activityId,Integer count,Long universityId);
 }
