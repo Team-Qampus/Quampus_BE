e40359cf5e39280e94af0d677c9193a92bd3f36c
tkv00 <tkv0098@gmail.com>
2025-03-08 18:38:06 +0900

    refact:lat/lon 오류 처리

--- src/main/java/swyp/qampus/openApi/LocationDto.java
+++ src/main/java/swyp/qampus/openApi/LocationDto.java
@@ -6,11 +6,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
+
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-@Builder
+@NoArgsConstructor
 public class LocationDto {
     private String 학교명;
     private String 위도;
     private String 경도;
+
+    @Builder
+    public LocationDto(String 학교명,String 위도,String 경도){
+        this.위도=위도;
+        this.경도=경도;
+        this.학교명=학교명;
+    }
 }
