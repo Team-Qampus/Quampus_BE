8d113f54ac6304b4ddf281296477426c2f03d810
tkv00 <tkv0098@gmail.com>
2025-03-09 16:06:28 +0900

    refact:activity/detail 토큰 제거

--- src/main/java/swyp/qampus/university/domain/response/UniversityRankResponseDto.java
+++ src/main/java/swyp/qampus/university/domain/response/UniversityRankResponseDto.java
@@ -4,8 +4,10 @@ import com.querydsl.core.annotations.QueryProjection;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
-@NoArgsConstructor
 @Schema(name = "학교랭킹 조회 Dto")
 public class UniversityRankResponseDto {
     private Long university_id;
@@ -19,7 +21,9 @@ public class UniversityRankResponseDto {
     //채택 수
     private Long choice_cnt;
 
-    private Location location;
+    private List<Double> location;
+
+    public UniversityRankResponseDto(){}
 
     public UniversityRankResponseDto(Long university_id, String university_name, Integer ranking, Long participant_count, Integer rate, Long choice_cnt,Double latitude,Double longitude) {
         this.university_id = university_id;
@@ -28,18 +32,8 @@ public class UniversityRankResponseDto {
         this.participant_count = participant_count;
         this.rate = rate;
         this.choice_cnt = choice_cnt;
-        this.location=new Location(longitude,latitude);
+        this.location = List.of(latitude, longitude);
     }
 
-    @Getter
-    @NoArgsConstructor
-    private static class Location{
-        Double longitude;
-        Double latitude;
 
-        public Location(Double longitude,Double latitude){
-            this.latitude=latitude;
-            this.longitude=longitude;
-        }
-    }
 }
