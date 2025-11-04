7505386ebfc4c56d804d488d0e220ff8a12df76a
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-04 14:04:25 +0900

    Update TestDto.java

--- src/main/java/swyp/qampus/test/TestDto.java
+++ src/main/java/swyp/qampus/test/TestDto.java
@@ -1,10 +1,14 @@
 package swyp.qampus.test;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Builder
 @Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class TestDto {
     private String name;
     private String major;
